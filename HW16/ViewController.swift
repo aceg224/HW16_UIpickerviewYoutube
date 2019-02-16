ed5a048be460d615eee82209987bab717087bcfa
aceg224 <aceg224@gmail.com>
2019-02-16 16:07:10 +0800

    commit

--- HW16/ViewController.swift
+++ HW16/ViewController.swift
@@ -8,7 +8,23 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController ,UIPickerViewDataSource,UIPickerViewDelegate{
+    
+    @IBOutlet weak var displayTextfield: UITextField!
+    var sumikkogurashi = ["角落生物","拉拉熊","鯨鯊先生","憂傷馬戲團","趴趴熊","烤焦麵包","靴下貓","海豹寶寶"]
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return sumikkogurashi.count
+    }
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return sumikkogurashi[row]
+    }
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        displayTextfield.text = sumikkogurashi[row]
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
